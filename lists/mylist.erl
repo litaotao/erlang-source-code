ee9c530518e345670da2f6ccec7a9296f181cb65
charles.li <charles.upboy@gmail.com>
2014-10-31 23:52:31 +0800

    fix

--- lists/mylist.erl
+++ lists/mylist.erl
@@ -8,13 +8,16 @@
       T :: term().
 
 nth(1, [H|_]) -> H;
-
+nth(N, []) when N > 0 ->
+	error_exceed;
+nth(N, [_|T]) when N > 1 ->
+	case T of
+		[] -> error_exceed;
+		_  ->
+    		nth(N - 1, T)
+    end.
 % nth(N, [_|T]) when N > 1 ->
-% 	case T of
-% 		[] -> error_exceed;
-% 		_  ->
-%     		nth(N - 1, T)
-%     end.
+% 	nth(N-1, T);
 
-nth(N, [_|T]) -> when N > 0, T=:=[] ->
-	error_exceed.
+% nth(N, [_|T]) when N > 0, T=:=[] ->
+% 	error_exceed.
